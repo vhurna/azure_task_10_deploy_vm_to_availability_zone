11579b2b04cd7c8a52df5a22ad9fd8d1c41937f7
mira <mkyshchak@gmail.com>
2025-05-06 23:42:08 +0300

    vm-in-av-zone

--- task.ps1
+++ task.ps1
@@ -1,46 +1,70 @@
-$location = "uksouth"
-$resourceGroupName = "mate-azure-task-10"
-$networkSecurityGroupName = "defaultnsg"
-$virtualNetworkName = "vnet"
-$subnetName = "default"
-$vnetAddressPrefix = "10.0.0.0/16"
-$subnetAddressPrefix = "10.0.0.0/24"
-$sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
-$vmName = "matebox"
-$vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
+$location                  = "uksouth"
+$resourceGroupName         = "mate-azure-task-10"
+$networkSecurityGroupName  = "defaultnsg"
+$virtualNetworkName        = "vnet"
+$subnetName                = "default"
+$vnetAddressPrefix         = "10.0.0.0/16"
+$subnetAddressPrefix       = "10.0.0.0/24"
+$sshKeyName                = "linuxboxsshkey"
+$sshKeyPublicKey           = Get-Content "~/.ssh/id_rsa.pub"
+$vmNameBase                = "matebox"
+$vmImage                   = "Ubuntu2204"
+$vmSize                    = "Standard_B1s"
 
-Write-Host "Creating a resource group $resourceGroupName ..."
+Write-Host "Creating resource group '$resourceGroupName' in $location..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
-New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+Write-Host "Creating NSG '$networkSecurityGroupName'..."
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 `
+    -SourceAddressPrefix * -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
+$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 `
+    -SourceAddressPrefix * -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow
 
+New-AzNetworkSecurityGroup `
+  -Name $networkSecurityGroupName `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+
+Write-Host "Creating VNet '$virtualNetworkName' with subnet '$subnetName'..."
 $subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
+New-AzVirtualNetwork `
+  -Name $virtualNetworkName `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -AddressPrefix $vnetAddressPrefix `
+  -Subnet $subnet
 
-New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
+Write-Host "Importing SSH public key resource '$sshKeyName'..."
+New-AzSshKey `
+  -Name $sshKeyName `
+  -ResourceGroupName $resourceGroupName `
+  -PublicKey $sshKeyPublicKey
 
-# Take a note that in this task VMs are deployed without public IPs and you won't be able
-# to connect to them - that's on purpose! The "free" Public IP resource (Basic SKU,
-# dynamic IP allocation) can't be deployed to the availability zone, and therefore can't 
-# be attached to the VM. Don't trust me - test it yourself! 
-# If you want to get a VM with public IP deployed to the availability zone - you need to use 
-# Standard public IP SKU (which you will need to pay for, it is not included in the free account)
-# and set same zone you would set on the VM, but this is not required in this task. 
-# New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel "random32987"
+# ────────────── Deploy TWO Linux VMs ──────────────
 
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $vmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $subnetName `
--VirtualNetworkName $virtualNetworkName `
--SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName 
-# -PublicIpAddressName $publicIpAddressName
+$commonVmParams = @{
+    ResourceGroupName   = $resourceGroupName
+    Location            = $location
+    VirtualNetworkName  = $virtualNetworkName
+    SubnetName          = $subnetName
+    SecurityGroupName   = $networkSecurityGroupName
+    Image               = $vmImage
+    Size                = $vmSize
+    SshKeyName          = $sshKeyName
+    # Видалено DisablePasswordAuthentication
+}
+
+Write-Host "Deploying VM '$vmNameBase-1' in zone 1..."
+New-AzVm @commonVmParams `
+  -Name "${vmNameBase}-1" `
+  -Zone 1
+
+Write-Host "Deploying VM '$vmNameBase-2' in zone 2..."
+New-AzVm @commonVmParams `
+  -Name "${vmNameBase}-2" `
+  -Zone 2
+
+Write-Host "`n✅ Successfully deployed two VMs ($vmNameBase-1, $vmNameBase-2) across Availability Zones 1 & 2."
